c815370934934971a651498c93f4c8effa4ac845
basantpatidar <basantpatidar33@gmail.com>
2018-07-16 00:20:10 -0400

    operators

--- gettingStarted/script.js
+++ gettingStarted/script.js
@@ -13,7 +13,7 @@ console.log(age);
 var fullAge = true;
 console.log(fullAge);   */
 
-
+/*
 //  Variables 2
 var name = 'John';
 var age = 26;
@@ -23,15 +23,50 @@ console.log(age + age);
 
 var job, isMarried;
 
-console.log(job);
+//console.log(job);
 
 job = 'teacher';
 isMarried = false;
 
-console.log(name + ' is a ' + age + ' years old ' + job + ' Is he married? ' + isMarried + '.');
+//console.log(name + ' is a ' + age + ' years old ' + job + ' Is he married? ' + isMarried + '.');
 
 age = 'thrity six';
 job = 'driver';
 
-console.log(name + ' is a ' + age + ' years old ' + job + ' Is he married? ' + isMarried + '.');
+//console.log(name + ' is a ' + age + ' years old ' + job + ' Is he married? ' + isMarried + '.');
+
+/*var lastName = prompt('what is the last name?');
+console.log(lastName);  
+
+alert(name + ' is a ' + age + ' years old ' + job + ' Is he married? ' + isMarried + '.');
+*/
+
+
+
+// Lecture :  Operators
+var now = 2018
+
+var birthYear = now - 26;
+//1996
+
+birthYear = now-26*2;
+
+console.log(birthYear);
+
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3+5)*4-6;
+//ageJohn = ageMark = 26;
+//ageJohn = 26;
+
+ageJohn ++;
+ageMark*= 2;
+ageMark = ageMark *2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
+
 
