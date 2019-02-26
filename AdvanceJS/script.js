33d487b863c4de372367a8e5a813176c9cab239f
basantpatidar <basantpatidar33@gmail.com>
2019-02-25 23:08:34 -0500

    Itarate

--- AdvanceJS/script.js
+++ AdvanceJS/script.js
@@ -59,3 +59,24 @@ function Bird(name) {
   let myHouse = new House(1);
   myHouse instanceof House;
   
+  //Iterate over all properties
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  for (let property in beagle){
+    if(beagle.hasOwnProperty(property)){
+    ownProps.push(property);
+  } else {
+  prototypeProps.push(property);
+  }
+  }
+  console.log(ownProps);
+  console.log(prototypeProps);
