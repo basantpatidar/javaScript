4939b1a77a81bb682dbfd9c828b100bfe3bba46d
basantpatidar <basantpatidar33@gmail.com>
2019-04-25 23:18:34 -0400

    object

---
+++ exerciseBasics/objectExercise.js
@@ -0,0 +1,54 @@
+let address = {
+    street: 222,
+    city: "New York",
+    zipcode: 123213
+};
+
+function showAddress(address){
+    for (let i in address){
+        console.log(i+" "+address[i]);
+    }
+}
+showAddress(address);
+
+// Factory Function
+function myAddress(){
+    return{
+        street: 222,
+    city: "New York",
+    zipcode: 123213
+    };
+}
+let factoryFunction = myAddress();
+console.log(factoryFunction);
+
+function ShowMyAddress(street,city, zipcode){
+    this.street = street;
+    this.city = city;
+    this.zipcode = zipcode;
+}
+let myNewAddress1 = new ShowMyAddress(123,"a",123400);
+let myNewAddress2 = new ShowMyAddress(123,"a",123410);
+
+
+function objectChecker(myNewAddress1, myNewAddress2){
+    let c = 0;
+    let l = Object.keys(myNewAddress1).length;
+    for (let key in myNewAddress1){
+        if(myNewAddress1[key] === myNewAddress2[key]){
+            c++;
+        if(c === l){
+            console.log("Same");
+            return true;
+             }
+             else{
+                 console.log("Both are different");
+                 return false
+                 
+             }
+        }
+    }
+}
+
+objectChecker(myNewAddress1,myNewAddress2);
+
