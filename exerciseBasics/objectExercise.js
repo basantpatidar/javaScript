8d00a0f292a85ffc435ef20261d837e28d9df1b4
basantpatidar <basantpatidar33@gmail.com>
2019-05-08 21:45:58 -0400

    proto

--- exerciseBasics/objectExercise.js
+++ exerciseBasics/objectExercise.js
@@ -437,3 +437,51 @@ function Stopwatch(){
 const sw = new Stopwatch();
 
 
+function StopwatchProto(){
+    let startTime, endTime, running, duration = 0;
+    
+
+   Object.defineProperty(this, 'duration', {
+       get: function(){return duration;},
+       set: function(value){duration = value;}
+   });
+
+   Object.defineProperty(this, 'startTime', {
+    get: function(){return startTime;}
+    });
+
+    Object.defineProperty(this, 'endTime', {
+    get: function(){return endTime;}
+    });
+
+    Object.defineProperty(this, 'running', {
+        get: function(){return running;}
+    });
+}
+
+const sw = new StopwatchProto();
+
+
+StopwatchProto.prototype.start = function(){
+    if(this.running)
+        throw new Error('Stopwatch is already running');
+    this.running = true;
+
+    this.startTime = new Date();
+};
+StopwatchProto.prototype.stop = function(){
+    if(!this.running)
+        throw new Error('Stopwatch is already stop');
+    
+    this.running = false;
+    this.endTime = new Date();
+    const seconds = (endTime.getTime() -startTime.getTime())/1000 ;
+    duration += seconds;
+};
+
+StopwatchProto.prototype.reset = function(){
+    this.startTime = null;
+    this.endTime = null;
+    this.running = false;
+    duration = 0;
+};
