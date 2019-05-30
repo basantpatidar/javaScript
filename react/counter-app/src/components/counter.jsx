3a0f0ba9f0259f1b398f7eb65fb888da7376f025
basantpatidar <basantpatidar33@gmail.com>
2019-05-30 03:22:00 -0400

    update app

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -4,29 +4,34 @@ class Counter extends Component {
     count: 0,
     tags: ["tag1","tag2","tag3"]
   };
+
+renderTags(){
+  if (this.state.tags.length === 0) return <p>There are no tags</p>
+}
+
   render() {
       
     return (
       <div>
-        <span className={this.getBadgeClasses()}>
+        {/* <span className={this.getBadgeClasses()}>
           {this.formateCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
         <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
       </div>
     );
   }
   //Creating a badge to count number of items in cart.
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes
-    }
-// If items in cart are 0 then shows "zero" else digit
-  formateCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+//     getBadgeClasses() {
+//         let classes = "badge m-2 badge-";
+//         classes += this.state.count === 0 ? "warning" : "primary";
+//         return classes
+//     }
+// // If items in cart are 0 then shows "zero" else digit
+//   formateCount() {
+//     const { count } = this.state;
+//     return count === 0 ? "Zero" : count;
+//   }
 }
 
 export default Counter;
