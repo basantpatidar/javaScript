ddaab08e6f5f62a0f82aed862cc443d98c8f5552
basantpatidar <basantpatidar33@gmail.com>
2019-05-31 18:42:06 -0400

    update

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -2,36 +2,43 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1","tag2","tag3"]
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-renderTags(){
-  if (this.state.tags.length === 0) return <p>There are no tags</p>
-}
+  // renderTags(){
+  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
+  //   return <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
+  // }
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1});
+  }
 
   render() {
-      
     return (
       <div>
-        {/* <span className={this.getBadgeClasses()}>
-          {this.formateCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
-        <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
+        <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags === 0 && "Please create a new tag!"}
+        {this.renderTags()} */}
       </div>
     );
   }
   //Creating a badge to count number of items in cart.
-//     getBadgeClasses() {
-//         let classes = "badge m-2 badge-";
-//         classes += this.state.count === 0 ? "warning" : "primary";
-//         return classes
-//     }
-// // If items in cart are 0 then shows "zero" else digit
-//   formateCount() {
-//     const { count } = this.state;
-//     return count === 0 ? "Zero" : count;
-//   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  // // If items in cart are 0 then shows "zero" else digit
+    formateCount() {
+      const { count } = this.state;
+      return count === 0 ? "Zero" : count;
+    }
 }
 
 export default Counter;
