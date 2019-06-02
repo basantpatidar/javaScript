92f4eb71269cf7f35e9f5a442cbac21f2c89c254
basantpatidar <basantpatidar33@gmail.com>
2019-06-01 22:43:48 -0400

    created as a component

--- react/counter-app/src/components/counter.jsx
+++ react/counter-app/src/components/counter.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  // renderTags(){
-  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
-  //   return <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
-  // }
   handleIncrement = () => {
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
   }
 
   render() {
+    console.log("props", this.props);
+    
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
@@ -23,20 +20,18 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {/* {this.state.tags === 0 && "Please create a new tag!"}
-        {this.renderTags()} */}
       </div>
     );
   }
   //Creating a badge to count number of items in cart.
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
   // // If items in cart are 0 then shows "zero" else digit
     formateCount() {
-      const { count } = this.state;
+      const { value: count } = this.state;
       return count === 0 ? "Zero" : count;
     }
 }
