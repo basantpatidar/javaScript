dbd0c38d193631828e57aa4e96f8e53cd00b989b
basantpatidar <basantpatidar33@gmail.com>
2019-06-06 23:24:38 -0400

    initial commit

---
+++ react/friends-list/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+const App = () => <PersonList />;
+
+const PersonList = () => {
+
+const people = [
+  {
+    img:20,name:'john',job:'developer'
+  },
+  {
+    img:24,name:'bob',job:'creator'
+  },
+  {
+    img:26,name:'tom',job:'engineer'
+  }
+];
+
+
+
+  return (
+    <section>
+      <Person person={people[0]} /> 
+      <Person person={people[1]} >
+        Lorem ipsum dolor sit consectetur adipisiciing elit. Deserunt Labiore.
+        </Person> 
+      
+      <Person person={people[2]}>
+      Lorem ipsum dolor sit consectetur adipisiciing elit. Deserunt Labiore. 
+      </Person> 
+
+    </section>
+  )
+};
+const Person = (props) => {
+  const {img,name,job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return <div className="person">
+    <img src={url} alt=""/>
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </div>
+     </div>;
+};
+
+
+export default App;
