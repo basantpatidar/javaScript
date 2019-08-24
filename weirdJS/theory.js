ffb8aa05e08e34eedc4974c3202eabbb1aee2934
basantpatidar <basantpatidar33@gmail.com>
2019-08-23 22:51:00 -0400

    update

--- weirdJS/theory.js
+++ weirdJS/theory.js
@@ -40,4 +40,49 @@
 10. Dynamic Typing:- JS has dynamic typing and JS engine dont need to know the type of variable. JS figures it out on runtime and 
                         type can be changed on runtime.
 
+11. Data types : -
+        A. Primitive: - Value type, a = 5; b = a; In this case be will have copied value of 'a' and memory address of 'b' will
+                        be defferent than memory address of 'a'. There are 6 types of Primitive data types 
+                            i.      Undifined : Lack of existence
+                            ii.     Null : Lack of existence
+                            iii.    String : Sequence of characters
+                            iv.     Number : floting point numbers
+                            v.      Boolean : true/false
+                            vi.     Symbol :  Introduces in ES6
 
+        B. Reference type : - Object and Array are reference type which points to same memory address even after mutation or after 
+        making a copy from One to another.
+                            
+
+12. JSON: - JavaScript Object Notation is simple a syntax used for exchanging information. JSON Syntax is like JS object but in JSON
+            both key and value are under quotes. JSON is faster and simpler than XML.
+            Using JSON.stringify(objLiteral); an Object can be converted into JSON syntax
+            while using JSON.parse(); as JSON can be converted into JS Onject. .
+
+ */
+
+ /*
+
+//13. Types of functions: 
+//Function Statement
+stmt();
+function stmt(){
+    console.log("This is function statement and this can be invoked before it is declared as functinons are hoisted.");
+}
+
+//Function Expression
+var expsn = function(){
+    console.log("This is function expression and this can only be invoked after it is declared or it will throw undefined error",
+                "as variables are not hoisted with values, they just get memory space with undefined");
+}
+expsn(); //This is the way function expression is invoked.
+
+// First class function : A function is passed as an argument to another function
+function firstClass(a){
+    a(); //Invoking function from argument.
+}
+firstClass(function(){
+    console.log("This function is passed as an argument to firstClass function as 'a'");
+});
+*/
+/*
